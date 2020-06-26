0d325108dfce0ea5d3c216db9f9ffb99e76430ec
8cw <54337835+8cw@users.noreply.github.com>
2020-06-26 12:04:44 +1200

    Initial Commit

---
+++ hackerSimulator/Project.vb
@@ -0,0 +1,14 @@
+﻿Module Project
+    Function myCustomCompareFunction(a As Integer, b As Integer) As Integer
+        Return a * b
+    End Function
+
+    Sub Main()
+        test.compare(AddressOf myCustomCompareFunction)
+        Console.ReadKey()
+
+        While (Not (Console.ReadKey(True).Key = ConsoleKey.S))
+            Console.WriteLine("press s")
+        End While
+    End Sub
+End Module
