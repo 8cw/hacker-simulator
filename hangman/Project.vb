c4ab231743071961a038cf741725deb8fbaae665
8cw <54337835+8cw@users.noreply.github.com>
2020-07-22 09:59:56 +1200

    add words

--- hangman/Project.vb
+++ hangman/Project.vb
@@ -1,5 +1,7 @@
 ﻿Module Project
     Sub Main()
+        Dim threeLetterWords As String() = {"arm", "aid", "air", "app", "bed", "box", "cot", "bus", "buy", "car", "cut", "cat", "cup", "toy", "dog", "hip", "die", "dad", "due", "egg", "era", "eye", "end", "fan", "fee", "fat", "hue", "fit", "gas", "god", "gun", "tub", "rug", "rob", "gym", "hit", "hot", "him", "her", "ice", "ide", "ink", "inn", "job", "joy", "kid", "key", "kit", "lab", "law", "leg", "log", "men", "met", "mix", "may", "net", "new", "sad", "fox", "now", "nor", "red", "off", "oil", "old", "out", "pet", "pay", "pro", "pin", "ran", "red", "rid", "row", "sad", "saw", "sea", "sky", "tax", "tab", "tip", "top", "ute", "uni", "vac", "vat", "vet", "war", "wax", "wet", "wee", "yam", "yip", "yok", "yum", "zip", "zit", "zoo", "zed"}
+        Dim fourletterWords As String() = {"aced", "ache", "ahoy", "aids", "babe", "baby", "bags", "bait", "calf", "came", "camp", "cars", "dabs", "dame", "data", "date", "edgy", "eery", "eggs", "envy", "face", "fade", "farm", "fart", "gale", "game", "gape", "glut"}
         Console.ReadKey()
     End Sub
 End Module
