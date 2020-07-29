aa03d5c9e9c03123ea5f85f7dd945881717d588d
8cw <54337835+8cw@users.noreply.github.com>
2020-07-30 10:21:34 +1200

    Add space message out util function

--- hangman/Util.vb
+++ hangman/Util.vb
@@ -5,4 +5,21 @@
         Question
         RequestAction
     End Enum
+
+    ''' <summary>
+    ''' Spaces a message out by inserting a space between each character
+    ''' </summary>
+    ''' <param name="message">The message to space out</param>
+    ''' <returns></returns>
+    Function spaceMessageOut(message As String) As String
+        Dim newMessage = ""
+
+        ' Space the message out
+        For Each c In message
+            newMessage += c + " "
+        Next
+
+        ' Trim the last space
+        Return newMessage.Substring(0, newMessage.Length - 1)
+    End Function
 End Module
